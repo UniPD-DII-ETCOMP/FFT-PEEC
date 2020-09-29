12c19c307c456e87f7767438ddfb182691245752
Riccardo Torchio <torchioriccardo92@gmail.com>
2020-09-29 09:45:45 +0200

    Update grid3dRT2.m

--- fun/myfun/grid3dRT2.m
+++ fun/myfun/grid3dRT2.m
@@ -3,6 +3,7 @@ L = length(x);
 M = length(y);
 N = length(z);
 r = zeros(L,M,N,3);
+try 
 parfor ix = 1:L
     xx = x(ix);
     for iy = 1:M
@@ -12,4 +13,17 @@ parfor ix = 1:L
             r(ix,iy,iz,:) = [xx yy zz];
         end
     end
+end
+catch
+for ix = 1:L
+    xx = x(ix);
+    for iy = 1:M
+        yy = y(iy);
+        for iz = 1:N
+            zz = z(iz);
+            r(ix,iy,iz,:) = [xx yy zz];
+        end
+    end
+end    
+end
 end
