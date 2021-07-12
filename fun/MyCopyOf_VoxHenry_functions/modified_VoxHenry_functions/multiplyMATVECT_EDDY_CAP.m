233c17f68947dcbf5996fabc47858f9c44f9e384
Riccardo Torchio <torchioriccardo92@gmail.com>
2021-07-12 13:01:13 +0200

    capacitive effects can now be removed

--- fun/MyCopyOf_VoxHenry_functions/modified_VoxHenry_functions/multiplyMATVECT_EDDY_CAP.m
+++ fun/MyCopyOf_VoxHenry_functions/modified_VoxHenry_functions/multiplyMATVECT_EDDY_CAP.m
@@ -4,7 +4,7 @@
 % https://github.com/acyucel/VoxHenry
 %%
 function [JOut_full] = multiplyMATVECT_EDDY_CAP(JIn0,CL,CP,z_realx,z_realy,...
-    z_realz,idxF,d,Aee,L,M,N,idxV,freq)
+    z_realz,idxF,d,Aee,L,M,N,idxV,freq,capacitive_effects_flag)
 dx = d(1); dy = d(2); dz = d(3);
 omega = 2*pi*freq;
 num_node=size(Aee,1); %num potential nodes, 
@@ -29,16 +29,20 @@ JOut(:,:,:,3) = (dz/(dx*dy)) .* z_realz .* JIn(:,:,:,3) + Jout3(1:L,1:M,1:N);
 JOut = JOut(idxF);
 JOut_full(1:num_curr) = JOut;
 JOut_full(1:num_curr) = JOut_full(1:num_curr) + (Aee.'*JIn0(num_curr+1:num_curr+num_node)) ;
-q = Aee*JIn0(1:num_curr);
-QIn = zeros(L,M,N);  
-QIn(idxV) = q;
-fJ = fftn(QIn(:,:,:),[LfN, MfN, NfN]);
-Jout = CP(:,:,:) .* fJ; % P*Q
-JOut = ifftn(Jout);
-JOut = JOut(1:L,1:M,1:N);
-JOut = JOut(idxV);
-JOut_full(num_curr+1:end) = JOut;
-JOut_full(num_curr+1:end) = ...
-    (JOut_full(num_curr+1:end) - (1j*omega*JIn0(num_curr+1:end)));
+if capacitive_effects_flag
+    q = Aee*JIn0(1:num_curr);
+    QIn = zeros(L,M,N);  
+    QIn(idxV) = q;
+    fJ = fftn(QIn(:,:,:),[LfN, MfN, NfN]);
+    Jout = CP(:,:,:) .* fJ; % P*Q
+    JOut = ifftn(Jout);
+    JOut = JOut(1:L,1:M,1:N);
+    JOut = JOut(idxV);
+    JOut_full(num_curr+1:end) = JOut;
+    JOut_full(num_curr+1:end) = ...
+        (JOut_full(num_curr+1:end) - (1j*omega*JIn0(num_curr+1:end)));
+else
+    JOut_full(num_curr+1:num_curr+num_node) = Aee*JIn0(1:num_curr);
+end
 end
 
