6db29eac832f7d7a6919d671b37f00eb322c7665
Riccardo Torchio <torchioriccardo92@gmail.com>
2022-03-08 10:40:13 +0100

    added comment

--- fortran/make.m
+++ fortran/make.m
@@ -3,3 +3,4 @@ close all
 clc
 
 mex -v -O -largeArrayDims -output computeGREEN_f90_mexed integration_fortran.f90 
+%mex -v -O -largeArrayDims -output computeGREEN_f90_mexed integration_fortran.F90 % IN CASE THE ABOVE does not work, change the extension from .f90 to .F90
